ffaa3543a56b0afea70be04555566e77d6a520f9
Vladislav Kovzov <fenix23707@gmail.com>
2022-03-07 17:26:56 +0300

    Added Subscribe dao and service

--- phone-network/src/main/java/by/vsu/kovzov/Main.java
+++ phone-network/src/main/java/by/vsu/kovzov/Main.java
@@ -1,33 +1,89 @@
 package by.vsu.kovzov;
 
 import by.vsu.kovzov.dao.OperationDao;
+import by.vsu.kovzov.dao.SubscriberDao;
 import by.vsu.kovzov.dao.file.OperationDaoFileImpl;
+import by.vsu.kovzov.dao.file.SubscriberDaoFileImpl;
 import by.vsu.kovzov.models.Phone;
 import by.vsu.kovzov.models.Subscriber;
 import by.vsu.kovzov.models.info.CallInfo;
 import by.vsu.kovzov.models.operations.CallOperation;
+import by.vsu.kovzov.models.operations.Operation;
+import by.vsu.kovzov.models.tariffs.Tariff;
 import by.vsu.kovzov.models.tariffs.impl.ATariff;
 import by.vsu.kovzov.services.OperationService;
+import by.vsu.kovzov.services.SubscriberService;
 import by.vsu.kovzov.services.impl.OperationServiceImpl;
+import by.vsu.kovzov.services.impl.SubscriberServiceImpl;
 
-import java.util.Date;
+import java.math.BigDecimal;
+import java.util.*;
 
+import static by.vsu.kovzov.Constants.OUTER_OPERATOR_CODES;
 import static by.vsu.kovzov.Constants.TARIFFS;
 
 public class Main {
+    private static List<Class<? extends Tariff>> tariffs = Arrays.asList(ATariff.class);
 
     private static OperationService operationService;
+
+    private static SubscriberService subscriberService;
 
     static {
         OperationDao operationDao = new OperationDaoFileImpl("data/operations.data");
         operationService = new OperationServiceImpl(operationDao);
+
+        SubscriberDao subscriberDao = new SubscriberDaoFileImpl("data/subscribers.data");
+        subscriberService = new SubscriberServiceImpl(subscriberDao);
     }
 
     public static void main(String[] args) {
-        Phone phone = new Phone("00375", "55", "1111111");
-        Subscriber subscriber = new Subscriber(phone, TARIFFS.get(ATariff.class));
-        CallOperation operation = new CallOperation(new CallInfo(subscriber, subscriber, new Date(), new Date(new Date().getTime() + 100 * 1000)));
-        operationService.save(operation);
+        initSubscribers();
+        initOperations();
+
         System.out.println(operationService.findAll());
+        System.out.println(subscriberService.findAll());
+    }
+
+    public static void initOperations() {
+        List<Subscriber> subscribers = subscriberService.findAll();
+        Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+            CallInfo info = new CallInfo(subscribers.get(0),
+                    subscribers.get(1 + random.nextInt(subscribers.size() - 1)),
+                    new Date(),
+                    new Date(System.currentTimeMillis() + 1000 * random.nextInt(60)));
+            Operation operation = new CallOperation(info);
+            operationService.save(operation);
+        }
+    }
+
+    public static void initSubscribers() {
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            BigDecimal balance = BigDecimal.valueOf(random.nextInt(500));
+            Subscriber subscriber = new Subscriber(getRandomPhone(), balance, randomTariff());
+            subscriberService.save(subscriber);
+        }
+    }
+
+    private static Phone getRandomPhone() {
+        Random random = new Random();
+
+        String countryCode = "0037" + (random.nextInt(4) == 3 ? "1" : "5");
+
+        List<String> codes = OUTER_OPERATOR_CODES;
+        String operationCode = codes.get(random.nextInt(codes.size()));
+
+        String number = "";
+        for (int i = 0; i < 7; i++) {
+            number += random.nextInt(10);
+        }
+
+        return new Phone(countryCode, operationCode, number);
+    }
+
+    private static Tariff randomTariff() {
+        return TARIFFS.get(tariffs.get(new Random().nextInt(tariffs.size())));
     }
 }
--- phone-network/src/main/java/by/vsu/kovzov/services/SubscriberService.java
+++ phone-network/src/main/java/by/vsu/kovzov/services/SubscriberService.java
@@ -1,8 +1,14 @@
 package by.vsu.kovzov.services;
 
+import by.vsu.kovzov.models.Phone;
 import by.vsu.kovzov.models.Subscriber;
 
+import java.util.List;
+
 public interface SubscriberService {
+    List<Subscriber> findAll();
+
+    Subscriber findByPhone(Phone phone);
 
     Subscriber save(Subscriber subscriber);
 }
