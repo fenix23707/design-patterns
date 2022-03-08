283daa7cfde6175621c86d35364fd9819903f804
Kalath19 <96651773+Kalath19@users.noreply.github.com>
2022-03-08 21:35:14 +0300

    Added test for B tariff Internet

--- phone-network/src/test/java/by/vsu/models/tariffs/InternetTariffTest.java
+++ phone-network/src/test/java/by/vsu/models/tariffs/InternetTariffTest.java
@@ -4,6 +4,8 @@ import by.vsu.models.Phone;
 import by.vsu.models.Subscriber;
 import by.vsu.models.info.InternetInfo;
 import by.vsu.models.tariffs.impl.ATariff;
+import by.vsu.models.tariffs.impl.BTariff;
+import by.vsu.models.tariffs.impl.GTariff;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -38,14 +40,31 @@ class InternetTariffTest {
                 ),
                 Arguments.of(
                         TARIFFS.get(ATariff.class),
-                        new InternetInfo(owner, 100, start, new Date(date + 49 * 60 * 1000)),
-                        BigDecimal.valueOf(((ATariff) TARIFFS.get(ATariff.class)).internetPrice)
+                        new InternetInfo(owner, 100, start, new Date(date + 45 * 60 * 1000)),
+                        BigDecimal.valueOf(((ATariff) TARIFFS.get(ATariff.class)).internetPrice * 45 / 60)
+                ),
+                Arguments.of(
+                        TARIFFS.get(ATariff.class),
+                        new InternetInfo(owner, 100, start, new Date(date + 30 * 60 * 1000)),
+                        BigDecimal.valueOf(((ATariff) TARIFFS.get(ATariff.class)).internetPrice * 30 / 60)
                 ),
                 Arguments.of(
                         TARIFFS.get(ATariff.class),
                         new InternetInfo(owner, 100, start, new Date(date + 120 * 60 * 1000)),
-                        BigDecimal.valueOf(((ATariff) TARIFFS.get(ATariff.class)).internetPrice*2)
+                        BigDecimal.valueOf(((ATariff) TARIFFS.get(ATariff.class)).internetPrice *  120 / 60)
+                ),
+                // B tariff
+                Arguments.of(
+                        TARIFFS.get(BTariff.class),
+                        new InternetInfo(owner, 50, start, new Date(date + 60 * 60 * 1000)),
+                        BigDecimal.valueOf(((BTariff) TARIFFS.get(BTariff.class)).firstPartInternetPrice)
+                ),
+                Arguments.of(
+                        TARIFFS.get(BTariff.class),
+                        new InternetInfo(owner, 100, start, new Date(date + 60 * 60 * 1000)),
+                        BigDecimal.valueOf(((BTariff) TARIFFS.get(BTariff.class)).firstPartInternetPrice + ((BTariff) TARIFFS.get(BTariff.class)).internetPrice)
                 )
+
         );
     }
 }
