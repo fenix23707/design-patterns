cc94abb354e35d5e8f4e25d0d6eefe9100ae6599
Kalath19 <96651773+Kalath19@users.noreply.github.com>
2022-03-08 21:59:06 +0300

    Added all test for Internet

--- phone-network/src/test/java/by/vsu/models/tariffs/InternetTariffTest.java
+++ phone-network/src/test/java/by/vsu/models/tariffs/InternetTariffTest.java
@@ -6,6 +6,7 @@ import by.vsu.models.info.InternetInfo;
 import by.vsu.models.tariffs.impl.ATariff;
 import by.vsu.models.tariffs.impl.BTariff;
 import by.vsu.models.tariffs.impl.GTariff;
+import by.vsu.models.tariffs.impl.VTariff;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -57,12 +58,49 @@ class InternetTariffTest {
                 Arguments.of(
                         TARIFFS.get(BTariff.class),
                         new InternetInfo(owner, 50, start, new Date(date + 60 * 60 * 1000)),
-                        BigDecimal.valueOf(((BTariff) TARIFFS.get(BTariff.class)).firstPartInternetPrice)
+                        BigDecimal.valueOf(((BTariff) TARIFFS.get(BTariff.class)).firstPartInternetPrice * 50)
                 ),
                 Arguments.of(
                         TARIFFS.get(BTariff.class),
                         new InternetInfo(owner, 100, start, new Date(date + 60 * 60 * 1000)),
-                        BigDecimal.valueOf(((BTariff) TARIFFS.get(BTariff.class)).firstPartInternetPrice + ((BTariff) TARIFFS.get(BTariff.class)).internetPrice)
+                        BigDecimal.valueOf(((BTariff) TARIFFS.get(BTariff.class)).firstPartInternetPrice * 50 + ((BTariff) TARIFFS.get(BTariff.class)).internetPrice*50)
+                ),
+                Arguments.of(
+                        TARIFFS.get(BTariff.class),
+                        new InternetInfo(owner, 203, start, new Date(date + 60 * 60 * 1000)),
+                        BigDecimal.valueOf(((BTariff) TARIFFS.get(BTariff.class)).firstPartInternetPrice * 50 + ((BTariff) TARIFFS.get(BTariff.class)).internetPrice*153)
+                ),
+                // V tariff
+                Arguments.of(
+                        TARIFFS.get(VTariff.class),
+                        new InternetInfo(owner, 50, start, new Date(date + 60 * 60 * 1000)),
+                        BigDecimal.valueOf(((VTariff) TARIFFS.get(VTariff.class)).firstPartInternetPrice * 50)
+                ),
+                Arguments.of(
+                        TARIFFS.get(VTariff.class),
+                        new InternetInfo(owner, 100, start, new Date(date + 60 * 60 * 1000)),
+                        BigDecimal.valueOf(((VTariff) TARIFFS.get(VTariff.class)).firstPartInternetPrice * 50 + ((VTariff) TARIFFS.get(VTariff.class)).internetPrice*50)
+                ),
+                Arguments.of(
+                        TARIFFS.get(VTariff.class),
+                        new InternetInfo(owner, 203, start, new Date(date + 60 * 60 * 1000)),
+                        BigDecimal.valueOf(((VTariff) TARIFFS.get(VTariff.class)).firstPartInternetPrice * 50 + ((VTariff) TARIFFS.get(VTariff.class)).internetPrice*153)
+                ),
+                // G tariff
+                Arguments.of(
+                        TARIFFS.get(GTariff.class),
+                        new InternetInfo(owner, 50, start, new Date(date + 60 * 60 * 1000)),
+                        BigDecimal.valueOf(((GTariff) TARIFFS.get(GTariff.class)).firstPartInternetPrice * 50)
+                ),
+                Arguments.of(
+                        TARIFFS.get(GTariff.class),
+                        new InternetInfo(owner, 120, start, new Date(date + 60 * 60 * 1000)),
+                        BigDecimal.valueOf(((GTariff) TARIFFS.get(GTariff.class)).firstPartInternetPrice * 100 + ((GTariff) TARIFFS.get(GTariff.class)).midlPartInternetPrice*20)
+                ),
+                Arguments.of(
+                        TARIFFS.get(GTariff.class),
+                        new InternetInfo(owner, 503, start, new Date(date + 60 * 60 * 1000)),
+                        BigDecimal.valueOf(((GTariff) TARIFFS.get(GTariff.class)).firstPartInternetPrice * 100 + ((GTariff) TARIFFS.get(GTariff.class)).midlPartInternetPrice * 400 +((GTariff) TARIFFS.get(GTariff.class)).internetPrice*3)
                 )
 
         );
