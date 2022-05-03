8ba07bff3e6b6465cd43b0c7f8b0d3d4fbc55819
Vladislav Kovzov <fenix23707@gmail.com>
2022-05-03 09:10:31 +0300

    Init project

---
+++ factory-method/src/main/java/by/vsu/kovzov/Runner.java
@@ -0,0 +1,41 @@
+package by.vsu.kovzov;
+
+import java.awt.BorderLayout;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class Runner {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {}
+        JFrame window = new JFrame("Main window");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(500, 400);
+        List<Person> persons = PersonXmlReader.readFromFile(Runner.class.getClassLoader().getResource("persons.xml").getFile());
+        PersonTableModel model = new PersonTableModel(persons);
+        JTable table = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(table);
+        window.add(scrollPane, BorderLayout.CENTER);
+        JPanel chooser = new JPanel(new BorderLayout());
+        JComboBox<String> comboBox = new JComboBox<>();
+        comboBox.addItem("by date");
+        comboBox.addItem("by day of week");
+        comboBox.addItem("by month");
+        chooser.add(comboBox, BorderLayout.CENTER);
+        JButton sortButton = new JButton("sort");
+        sortButton.addActionListener(new SortButtonListener(comboBox, model));
+        chooser.add(sortButton, BorderLayout.EAST);
+        window.add(chooser, BorderLayout.NORTH);
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+    }
+}
